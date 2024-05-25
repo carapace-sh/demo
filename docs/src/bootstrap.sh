9111ee4092183017d690c58266c7dceef4886f11
rsteube <rsteube@users.noreply.github.com>
2024-05-25 17:54:03 +0200

    tmp

--- docs/src/bootstrap.sh
+++ docs/src/bootstrap.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
 pkg update -y
+
+pkg install -y bat \
+               elvish \
+               eza \
+               fish \
+               git \
+               gh \
+               golang \
+               gopls \
+               helix \
+               htop \
+               python \
+               ripgrep \
+               starship \
+               tig \
+               vivid \
+               which \
+               wget \
+               zsh
+
+pip install httpie \
+            xonsh
---
+++ docs/src/setup.md
@@ -0,0 +1,17 @@
+# Setup
+
+## F-Droid
+
+Install [F-Droid](https://f-droid.org/).
+
+## Termux
+
+Install [Termux](https://f-droid.org/en/packages/com.termux/) and [Termux:Styling](https://f-droid.org/en/packages/com.termux.styling/).
+
+## App Pinning
+
+Enable `App Pinning`.
+
+## Timeout
+
+Set `Screen timeout` to `2 minutes`.
