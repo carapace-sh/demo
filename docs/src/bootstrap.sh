0ea2ae301d8b5d8cbaebe727f8dd1a65fca5af38
rsteube <rsteube@users.noreply.github.com>
2024-05-25 19:27:43 +0200

    tmp

--- docs/src/bootstrap.sh
+++ docs/src/bootstrap.sh
@@ -34,3 +34,26 @@ add_newline = false
 [shell]
 disabled = false
 " > ~/.config/starship.toml
+
+# elvish
+mkdir --parents ~/.config
+echo "\
+set paths = [ ~/.local/bin ~/go/bin \$@paths ]
+
+
+set-env CARAPACE_MATCH 1
+set edit:completion:matcher[argument] = {|seed| edit:match-prefix \$seed &ignore-case=\$true }
+
+set-env CARAPACE_BRIDGES zsh,fish,bash
+eval (carapace _carapace|slurp)
+
+unset-env STARSHIP_SHELL
+set-env STARSHIP_SHELL elvish
+set-env SHELL elvish
+set-env EDITOR hx
+set edit:prompt = { starship prompt }
+set E:LS_COLORS = (vivid generate dracula)
+
+set edit:rprompt = { echo '' }
+" > ~/.config/elvish/rc.elv
+
